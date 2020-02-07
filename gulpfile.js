c21740a3e755b3b5c62580f30d3b3fc6a17c3e8f
jdcastano <juan.castano@openintl.com>
2020-02-07 07:25:08 -0500

    Se agrega la instrucción watch al gulp para que compile automaticamente cuando se detecte un cambio en los archivos dentro de la carpeta src

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,22 @@ const browserify = require("browserify");
 const source = require("vinyl-source-stream");
 const connect = require("gulp-connect");
 
+const isBuild = process.argv.includes("--build");
+const isStart = process.argv.includes("--start");
+const isWatch = process.argv.includes("--watch");
+
 gulp.task("copy", done => {
   gulp.src("./src/index.html").pipe(gulp.dest("./build"));
   done();
 });
 
-/*gulp.task("server", done => {
+gulp.task("server", done => {
   connect.server({
     root: ["./build"],
     port: 8080
   });
   done();
-});*/
+});
 
 gulp.task("build", done => {
   browserify({
@@ -32,5 +36,15 @@ gulp.task("build", done => {
   done();
 });
 
-gulp.task("default", gulp.series("copy", "build"));
-//gulp.task("default", gulp.series("server"));
+gulp.task("watch", done => {
+  gulp.watch("./src", gulp.series("copy", "build"));
+});
+
+let serie;
+if (isStart) serie = gulp.series("server");
+if (isBuild) serie = gulp.series("copy", "build");
+if (isWatch) serie = gulp.series("copy", "build", "server", "watch");
+
+gulp.task("default", serie);
+
+//Tarea... Hacer que la tarea watch refresque el navegador cuando termine
